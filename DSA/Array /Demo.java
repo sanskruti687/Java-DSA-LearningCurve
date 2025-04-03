6ad64c6f4c62c33c2dfac6b843fb51c97822fbd7
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-03 21:42:10 +0530

    perform basic array program

---
+++ DSA/Array /Demo.java
@@ -0,0 +1,19 @@
+// writing a basic java array program
+
+public class Demo
+{
+public static void main(String args[])
+{
+int Num [] = new int[5];
+Num[0] = 10;
+Num[1] = 44;
+Num[2] = 140;
+Num[3] = 120;
+Num[4] = 320;
+
+System.out.println(Num[0]);
+System.out.println(Num[2]);
+
+System.out.println(Num[3] + " " + Num[4]);
+}
+}
