fdb1fee5d8432240597f67f2b751c4f7127f9fc8
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-03 20:14:08 +0530

    perform array

--- DSA/Array /arrayEx.java
+++ DSA/Array /arrayEx.java
@@ -1,2 +1,18 @@
-// given an array of marks of student if the marks of any student is less than 35 print its roll number [roll number refer to the index of array]
+// Java Program to Declare, Initialize, and Print an Array
+import java.util.Arrays;
 
+public class arrayEx {
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array elements using a loop
+        System.out.println("Array elements:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        // Print the array using Arrays.toString() method
+        System.out.println("\nArray using toString(): " + Arrays.toString(numbers));
+    }
+}
