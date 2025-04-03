797c55204693cbb32b40211d200b55b53b73dab5
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-03 22:36:37 +0530

    added UserInput Program

---
+++ DSA/Array /UserInput.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class UserInput
+{
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter the size of the element");
+    int size=sc.nextInt();
+    int number[]=new int[size];
+
+    for(int i=0;i<=size;i++)
+    {
+        System.out.println("enter the element");
+        number[i]=sc.nextInt();
+        System.out.println(number[i]);
+
+    }
+}
+
+}
