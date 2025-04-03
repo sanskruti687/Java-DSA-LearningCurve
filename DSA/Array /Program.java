054e5c81e4f65cf4e3c3c3effce7ad1fe7dad16c
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-03 21:52:50 +0530

    perform basic arra program by using for loop

---
+++ DSA/Array /Program.java
@@ -0,0 +1,16 @@
+// perform another method of the java declaration and element insert by using for loop 
+
+public class Program 
+{
+public static void main(String args[])
+{
+String StudentName []= {"Sandip ","Aryaman","Kunal","Krushna","Dipti"};
+
+for(int i =0;i<=6;i++)
+{
+    System.out.println(StudentName[i]);
+}
+
+}
+
+}
