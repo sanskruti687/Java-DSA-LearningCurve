dbbdaf8504124e0472a8b8de0e74a8a5f6df9343
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-04 14:58:01 +0530

    create a folder for array progra

---
+++ DSA/Array /UserExample.java
@@ -0,0 +1,15 @@
+public class UserExample
+{
+    public static void main(String args[])
+    {
+        int Number[]={2,3,4,5,6,7,7,8,8,22,33,44,90};
+        for(int i=0;i<Number.length;i++)
+        {
+            if(Number[i]<8)
+            {
+                System.out.println(Number[i]);
+            }
+
+        }
+    }
+}
