cab0445752597f3733460292944a9287cb73d5da
sanskruti687 <sanskrutipawar.sits.it@gmail.com>
2025-04-05 23:14:32 +0530

    created a program for sum of array element

---
+++ DSA/Array-DSA/SumOfArray.java
@@ -0,0 +1,18 @@
+ // Find the Sum of All Elements in an Array
+ class SumOfArray
+ {
+    public static void main(String args[])
+    {
+        int arr[]={2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+             
+            
+        }
+  System.out.println("Sum of array elements: " + sum);
+    
+ }
+
+ }
